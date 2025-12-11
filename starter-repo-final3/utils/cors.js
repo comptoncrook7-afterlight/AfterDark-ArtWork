56b8de967a07abf48bbc93402a5f0aef418a43fb
steven butler <comptoncrook7@gmail.com>
2025-12-11 21:54:50 +0000

    clean project: remove nested duplicate folder causing Vercel build failures

--- starter-repo-final3/utils/cors.js
+++
@@ -1,6 +0,0 @@
-
-export function allowCors(res) {
-  res.setHeader('Access-Control-Allow-Origin', process.env.NEXT_PUBLIC_SITE_URL || '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,x-admin-secret')
-}
